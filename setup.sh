5c19e9c4cfe4f4b3a4e2fc99327bf7f975cbc3d9
Caleb Noble <uniqisuniq@outlook.com>
2021-07-29 19:52:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip3 install jprq
 mkdir iso && cd iso && wget http://mirrors.acm.wpi.edu/archlinux/iso/2021.07.01/archlinux-2021.07.01-x86_64.iso
-python3 -m http.server --directory=iso & jprq 8000 -s=archlinux
+# python3 -m http.server --directory=iso & jprq 8000 -s=archlinux
